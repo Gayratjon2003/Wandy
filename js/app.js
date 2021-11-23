2fce94c1fc83375c504bc9a8e0c83cd01b76f130
Gayratjon2003 <gayratjonabdijobborov@gmail.com>
2021-11-23 09:10:51 +0500

    home page deyarli tugadi carusel qoldi 1ta

---
+++ js/app.js
@@ -0,0 +1,42 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+
+
+  const swiper1 = new Swiper('.hyper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.hyper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hyper-button-next',
+      prevEl: '.hyper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.hyper-scrollbar',
+    },
+  });
